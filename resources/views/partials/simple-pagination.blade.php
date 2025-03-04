5f15faa2878235ad01c690e9b7d7817dbb145136
seppcaspar <caspar.sepp@tptlive.ee>
2025-03-04 18:25:10 +0200

    simple-pagination ümber tehtud/ kasutades daisyuid

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,18 +1,18 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagination">
+        <ul class="pagination flex justify-center">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                <li class="disabled" aria-disabled="true"><span class="btn btn-sm btn-outline-secondary">&laquo; @lang('pagination.previous')</span></li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="btn btn-sm btn-outline-secondary" style="padding: 0.5rem 1rem;">&laquo; @lang('pagination.previous')</a></li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next" class="btn btn-sm btn-outline-secondary" style="padding: 0.5rem 1rem;">@lang('pagination.next') &raquo;</a></li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                <li class="disabled" aria-disabled="true"><span class="btn btn-sm btn-outline-secondary" style="padding: 0.5rem 1rem;">@lang('pagination.next') &raquo;</span></li>
             @endif
         </ul>
     </nav>
